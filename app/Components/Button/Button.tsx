e34a8412877780a011d777edbb102f79061ec7ec
Gabriel Maia <gabrielsouzamaia99@gmail.com>
2024-02-10 21:30:57 -0300

    Adding some cjanges to Modal, Tasks. Made TaskItems

--- app/Components/Button/Button.tsx
+++ app/Components/Button/Button.tsx
@@ -54,6 +54,20 @@ const ButtonStyled = styled.button`
     cursor: pointer;
 
     transition: all 0.55s ease-in-out;
+
+    i{
+        margin-right: 1rem;
+        color: ${(props) => props.theme.colorGrey2};
+        font-size: 1.5rem;
+        transition: all 0.55ss ease-in-out;
+    }
+
+    &:hover{
+        color: %{(props) => props.theme.colorGrey0};
+        i{
+            color: ${(props) => props.theme.colorGrey0};
+        }
+    }
 `;
 
 export default Button;
