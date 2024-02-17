9d2b74b1f28313251e27efd4fd9e6ef5af7f1e0c
Gabriel Maia <gabrielsouzamaia99@gmail.com>
2024-02-16 23:28:22 -0300

    Adding more features

--- app/Components/Button/Button.tsx
+++ app/Components/Button/Button.tsx
@@ -36,12 +36,22 @@ function Button({
     return (
         <ButtonStyled
             type={type}
-
-        >
+            style={{
+                background: background,
+                padding: padding || "0.5rem 1rem",
+                borderRadius: borderRad || "0.5rem",
+                fontWeight: fw || "500",
+                fontSize: fs,
+                border: border || "none",
+                color: color || theme.colorGrey0,
+            }}
+            theme={theme}
+            onClick={click}
+          >
             {icon && icon}
             {name}
         </ButtonStyled>
-    )
+    );
 
 }
 
